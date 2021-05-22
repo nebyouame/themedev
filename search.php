a5c64fc98333dcd03f190fd1af259cb7ed52f85e
nebyouame <56713138+nebyouame@users.noreply.github.com>
2021-05-22 16:46:08 +0300

    Update search.php

--- search.php
+++ search.php
@@ -4,50 +4,58 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
  *
- * @package custom_theme
+ * @package DarkTheme
  */
-
 get_header();
 ?>
+<?php get_template_part('template-parts/bread'); ?>
+   
+<?php if(have_posts()):?> 
+	
+<section class="blog-style-one sec-pad blog-page mrmain_blog mrindex">
+    <div class="container">
+        <div class="row">
+			<?php if(is_active_sidebar('sidebar-left')) : ?> 
+			<div class="content-side col-lg-8 col-md-8 col-sm-12 col-xs-12 ">
+			<?php  else : ?>
+			<div class="content-side col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+			<?php  endif ; ?> 
+				   
+				<div id="primary" class="content-area">
+                    <main id="main" class="blog_list site-main ">
+				
+				<?php if ( have_posts() ) :
+                  while( have_posts() ): the_post();?> 
+				   <div id="post-<?php the_ID(); ?>" <?php post_class();?>>
+                  <?php get_template_part( 'template-parts/content' ); ?>
+                        <!-- blog post item -->
+                  </div><!-- End Post --> 
+				<?php endwhile;?>
+					<div class="dope-pagination text-center">
+						<?php the_posts_pagination(); ?>
+                    </div>
+			
+			<?php else :
+get_template_part( 'template-parts/content', 'none' ); endif;?>
 
-	<main id="primary" class="site-main">
+                        </main><!-- #main -->
+                    </div>
+                </div>			
+				<!-- sidebar area -->
+			<?php if(is_active_sidebar('sidebar-left')) { ?>
+			<div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="sidebar">
+				<?php dynamic_sidebar('sidebar-left'); ?>
+				</div>
+			</div>
+			<?php } ?>
+            <!--Sidebar-->
+				
+            </div>
+        </div>
+    </section>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'custom-theme' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php else : ?>
+<?php get_template_part( 'template-parts/content', 'search' ); ?>
+<?php endif; ?>	
+<?php get_footer(); ?>
