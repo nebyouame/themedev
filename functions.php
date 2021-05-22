e1428f440843fe4d4efc172f9810b99d36e8a5b4
nebyouame <56713138+nebyouame@users.noreply.github.com>
2021-05-22 16:40:18 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,180 +1,139 @@
 <?php
-/**
- * custom theme functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package custom_theme
- */
 
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
-}
+	if ( ! function_exists( 'DarkTheme_setup' ) ) :
+		function DarkTheme_setup() {
 
-if ( ! function_exists( 'custom_theme_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function custom_theme_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on custom theme, use a find and replace
-		 * to change 'custom-theme' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'custom-theme', get_template_directory() . '/languages' );
+	        // Make theme available for translation.
+			load_theme_textdomain( 'DarkTheme', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+			// Add default posts and comments RSS feed links to head.
+			add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+			// Let WordPress manage the document title.
+			add_theme_support( 'title-tag' );
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			array(
-				'menu-1' => esc_html__( 'Primary', 'custom-theme' ),
-			)
-		);
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			)
-		);
+			// Enable support for Post Thumbnails on posts and pages.
+			add_theme_support( 'post-thumbnails' );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'custom_theme_custom_background_args',
+			// Enable Wide angle image in gutenberg
+
+			add_theme_support( 'align-wide' );
+
+			// This theme uses wp_nav_menu() in one location.
+			register_nav_menus( array(
+				'main_menu' => esc_html__( 'Main Menu', 'DarkTheme' ),
+			) );
+
+			/*
+			 * Switch default core markup for search form, comment form, and comments
+			 * to output valid HTML5.
+			 */
+			add_theme_support(
+				'html5',
 				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
+					'comment-form',
+					'comment-list',
+					'caption',
 				)
-			)
-		);
+			);
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+			// Set up the WordPress core custom background feature.
+			add_theme_support( 'custom-background', apply_filters( 'DarkTheme_custom_background_args', array(
+				'default-color' => 'f4f7f6',
+			) ) );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
+			// Add theme support for selective refresh for widgets.
+			add_theme_support( 'customize-selective-refresh-widgets' );
+
+			// Custom Thumbnail Image Size
+			add_image_size("DarkTheme-landscape",730,400,true);
+
+			//Add support for core custom logo.
+			add_theme_support( 'custom-logo', array(
+				'height'      => 94,
+				'width'       => 200,
 				'flex-width'  => true,
 				'flex-height' => true,
-			)
-		);
+			) );
+		}
+	endif;
+	add_action( 'after_setup_theme', 'DarkTheme_setup' );
+	/*
+	 * This theme styles the visual editor to resemble the theme style,
+	 * specifically font, colors, and column width.
+ 	 */
+	add_editor_style( array( 'assets/css/editor-style.css', DarkTheme_fonts_url() ) );
+	
+	
+	/**
+	 * Set the content width in pixels, based on the theme's design and stylesheet.
+	 *
+	 * Priority 0 to make it available to lower priority callbacks.
+	 *
+	 */
+	function DarkTheme_content_width() {
+		$GLOBALS['content_width'] = apply_filters( 'DarkTheme_content_width', 640 );
 	}
-endif;
-add_action( 'after_setup_theme', 'custom_theme_setup' );
+	add_action( 'after_setup_theme', 'DarkTheme_content_width', 0 );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function custom_theme_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'custom_theme_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'custom_theme_content_width', 0 );
+	/**
+	 * Enqueue scripts and styles.
+	 */
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function custom_theme_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'custom-theme' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'custom-theme' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
+	require get_template_directory() .'/inc/theme_styles_scripts.php';
+
+	/**
+	 * DarkTheme Widgets.
+	 */
+	require get_template_directory() .'/inc/theme_widgets.php';
+
+	/**
+	 * Custom template tags for this theme.
+	 */
+	require get_template_directory() . '/inc/template-tags.php';
+
+	/**
+	 * Functions which enhance the theme by hooking into WordPress.
+	 */
+	require get_template_directory() . '/inc/template-functions.php';
+
+	/**
+	 * Custom Functions for the Theme.
+	 */
+	require get_template_directory() . '/inc/custom-functions.php';
+
+	/**
+	 * Nav Walker bootstrap walker
+	 */
+
+	require get_template_directory() .'/inc/bootstrap_walker.php';
+	/**
+	 * Customizer additions.
+	 */
+	require get_template_directory() . '/inc/customizer/customizer.php';
+
+	/**
+	 * Implement the Custom Header feature.
+	 */
+	require get_template_directory() . '/inc/customizer/custom-header.php';
+
+	/**
+	 * Load Jetpack compatibility file.
+	 */
+	if ( defined( 'JETPACK__VERSION' ) ) {
+		require get_template_directory() . '/inc/compatibility/jetpack.php';
+	}
+
+function DarkTheme_fonts_url(){
+	$font_url = '';
+	$font_family = array();
+	$font_family[] = 'Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i';
+
+	$query_args = array(
+		'family'	=> rawurlencode(implode('|',$font_family)),
 	);
+	$font_url = add_query_arg($query_args,'//fonts.googleapis.com/css');
+	return $font_url;
 }
-add_action( 'widgets_init', 'custom_theme_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function custom_theme_scripts() {
-	wp_enqueue_style( 'custom-theme-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'custom-theme-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'custom-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'custom_theme_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
