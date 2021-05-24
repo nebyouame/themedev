e8d80613f57fc2e16524b382096e9fd147d0fffb
ermiasashebr <56713586+ermiasashebr@users.noreply.github.com>
2021-05-24 07:08:59 +0300

    Update theme_widgets.php

--- inc/theme_widgets.php
+++ inc/theme_widgets.php
@@ -5,7 +5,7 @@
 	register_sidebar(array(
 	  'name' => esc_html__( 'Default Sidebar', 'DarkTheme' ),
 	  'id' => 'sidebar-left',
-	  'description' => esc_html__( 'Widgets in this area will be shown on the right-hand side.', 'DarkTheme' ),
+	  'description' => esc_html__( 'Those are right hand side widget displayed', 'DarkTheme' ),
 	  'before_widget'=>'<div id="%1$s" class="mrside sidebar blog-sidebar widget sidebar-widget %2$s "><div class="widget-content">',
 	  'after_widget'=>'</div></div>',
 	  'before_title' => '<div class="sidebar-title"><h4>',
@@ -15,7 +15,7 @@
 	  register_sidebar(array(
 		  'name' => esc_html__( 'Main Footer Sidebar', 'DarkTheme' ),
 		  'id' => 'footer-sidebar',
-		  'description' => esc_html__( 'Widgets in this area will be shown in Footer Area.', 'DarkTheme' ),
+		  'description' => esc_html__( 'Those are footer area widget displayed.', 'DarkTheme' ),
 		  'before_widget'=>'<div id="%1$s" class="mrfooter col-md-4 col-sm-4 col-xs-12 footer-column footer-widget %2$s">',
 		  'after_widget'=>'</div>',
 		  'before_title' => '<h2 class="widget-title footer_title foote_title_one">',
