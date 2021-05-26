fabf502f22c48dbfd772569740d16a4a59157428
Tinbit77 <83236465+Tinbit77@users.noreply.github.com>
2021-05-26 10:43:36 +0300

    Add files via upload

---
+++ tag.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package DarkTheme
+ */
+get_header();
+?>
+<?php get_template_part('template-parts/bread'); ?>
+	
+<section class="blog-style-one sec-pad blog-page mrmain_blog mrindex">
+    <div class="container">
+        <div class="row">
+			<?php if(is_active_sidebar('sidebar-left')) : ?> 
+			<div class="content-side col-lg-8 col-md-8 col-sm-12 col-xs-12 ">
+			<?php  else : ?>
+			<div class="content-side col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+			<?php  endif ; ?> 
+				   
+				<div id="primary" class="content-area">
+                    <main id="main" class="blog_list site-main ">
+				
+				<?php if ( have_posts() ) :
+                  while( have_posts() ): the_post();?> 
+				   <div id="post-<?php the_ID(); ?>" <?php post_class();?>>
+                  <?php get_template_part( 'template-parts/content' ); ?>
+                        <!-- blog post item -->
+                  </div><!-- End Post --> 
+				<?php endwhile;?>
+					<div class="dope-pagination text-center">
+						<?php the_posts_pagination(); ?>
+                    </div>
+			
+			<?php else :
+get_template_part( 'template-parts/content', 'none' ); endif;?>
+
+
+	
